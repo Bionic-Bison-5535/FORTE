a1925aeafec0acc07cbeab79379634abf77afdbb
Wesley McGinn <wesleymcginn1@gmail.com>
2024-02-21 16:23:31 -0500

    Find official offset angles

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -20,7 +20,7 @@ public class Robot extends TimedRobot {
     private final SendableChooser<String> getMoreDropdown = new SendableChooser<>();
     String noteToGet, getMoreNotes;
     
-    Wesswerve go = new Wesswerve(14, 15, 16, 17, 20, 21, 22, 23, 10, 11, 12, 13, 44, 182, 204, 335);
+    Wesswerve go = new Wesswerve(14, 15, 16, 17, 20, 21, 22, 23, 10, 11, 12, 13, 358, 225, 159, 250);
     Controls c1 = new Controls(0, 0.1);
     Controls c2 = new Controls(1, 0.1);
     Tim matchTimer = new Tim();
