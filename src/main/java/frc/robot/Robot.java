69d04657e681e82a88a17cd2e2e656f1981d0a91
Wesley McGinn <wesleymcginn1@gmail.com>
2024-02-21 18:00:03 -0500

    Add intake stuff and some launch stuff 4 raw mode

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -3,6 +3,7 @@ package frc.robot;
 import java.lang.Math;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.systems.*;
@@ -18,19 +19,21 @@ public class Robot extends TimedRobot {
     private final SendableChooser<String> noteDropdown = new SendableChooser<>();
     private final SendableChooser<String> getMoreDropdown = new SendableChooser<>();
     String noteToGet, getMoreNotes;
-    
+
     Wesswerve go = new Wesswerve(14, 15, 16, 17, 20, 21, 22, 23, 10, 11, 12, 13, 358, 225, 159, 250);
     Controls c1 = new Controls(0, 0.1);
     Controls c2 = new Controls(1, 0.1);
     Tim matchTimer = new Tim();
     Navx navx = new Navx();
     Lights leds = new Lights(30);
-    Motor intakeMotor = new Motor(5, true, false, 1);
-    Motor aimMotor = new Motor(24, true, false, 0.5);
+    Motor in = new Motor(5, true, true, 1);
+    Motor aimMotor = new Motor(24, false, false, 0.5);
     Motor rightThruster = new Motor(7, false, false, 1);
     Motor leftThruster = new Motor(8, false, true, 1);
     Motor feedMotor = new Motor(9, false, false, 1);
     Launch launcher = new Launch(leftThruster, rightThruster, feedMotor, aimMotor, 25, 1);
+
+    boolean intaking = false;
 
     @Override
     public void robotInit() {
@@ -80,6 +83,19 @@ public class Robot extends TimedRobot {
             if (c1.start() || c2.start()) {
                 mode = "smart";
             }
+            if (c1.a() || c2.a()) {
+                intaking = true;
+                launcher.intake();
+            }
+            if (c1.b() || c2.b()) {
+                intaking = false;
+                launcher.stopIntake();
+            }
+            if (launcher.intakeStage == 0) {
+                if (c1.onPress(Controls.LEFT)) {
+                    launcher.LAUNCHstart();
+                }
+            }
         } else if (mode == "smart") {
             go.swerve(Math.pow(c1.stick(1), 3), Math.pow(c1.stick(0), 3), Math.pow(c1.stick(4), 3), navx.yaw());
             if (c1.back() || c2.back()) {
@@ -105,7 +121,7 @@ public class Robot extends TimedRobot {
             leds.orange();
         } else if (DriverStation.getMatchTime() < 20) {
             leds.turquoise();
-        } else if (/*intaking note*/false) {
+        } else if (launcher.iseenote()) {
             leds.yellow();
         } else {
             leds.allianceColor();
@@ -114,6 +130,11 @@ public class Robot extends TimedRobot {
         // Update Systems:
         go.update();
         launcher.update();
+        if (intaking) {
+            in.set(0.7);
+        } else {
+            in.set(0);
+        }
         
     }
 
@@ -129,12 +150,15 @@ public class Robot extends TimedRobot {
     public void testInit() {
         matchTimer.reset();
         leds.white();
+
+        aimMotor.posMode = true;
     }
 
     @Override
     public void testPeriodic() {
+        aimMotor.goToPos -= c1.stick(5);
+        aimMotor.update();
         SmartDashboard.putNumber("Aim Value", launcher.aimPos());
-        aimMotor.set(c1.stick(1) + c2.stick(1));
     }
 
 }
