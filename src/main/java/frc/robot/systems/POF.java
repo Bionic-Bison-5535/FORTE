39f473eb02bca6acf0f6d5bd828fb9f9f12302f7
Wesley McGinn <wesleymcginn1@gmail.com>
2024-02-23 11:57:56 -0500

    Create POF.java

---
+++ src/main/java/frc/robot/systems/POF.java
@@ -0,0 +1,21 @@
+package frc.robot.systems;
+
+public class POF {
+
+    public Limelight cam;
+    public double[] posData;
+    public double x, y, yaw;
+
+    public POF(Limelight AprilTagCam) {
+        cam = AprilTagCam;
+        cam.activate();
+    }
+
+    public void update() {
+        posData = cam.allPosData();
+        x = posData[0];
+        y = posData[1];
+        yaw = posData[5];
+    }
+
+}
