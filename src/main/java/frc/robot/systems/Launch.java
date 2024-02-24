de372bcc7082ad77da30a8333055bd750633503c
Wesley McGinn <wesleymcginn1@gmail.com>
2024-02-23 19:35:22 -0500

    Pull note back before launching

--- src/main/java/frc/robot/systems/Launch.java
+++ src/main/java/frc/robot/systems/Launch.java
@@ -19,6 +19,8 @@ public class Launch {
         public static double amp = 88.14;
         /** Position for scoring in speaker while pressed up against subwoofer */
         public static double closeup = 15;
+        public static double min = -21;
+        public static double max = 120;
     }
 
 	public Launch(Motor LaunchLeftMotor, Motor LaunchRightMotor, Motor FeedMotor, Motor AimMotor, int aimCoderID, int sensorPin) {
@@ -30,16 +32,23 @@ public class Launch {
         aimCoder = new CANcoder(aimCoderID);
         aimMotor.setEnc(aimCoder.getPosition().getValue()*182);
         aimMotor.goTo(pos.intake);
+        feed.pwr = 3;
 	}
 
     public double aimPos() {
         return aimMotor.getEnc();
     }
-
+    
     public void aim(double encValue) {
-        aimMotor.goTo(encValue);
+        if (encValue > pos.min && encValue < pos.max) {
+            aimMotor.goTo(encValue);
+        }
     }
 
+    public void changeAim(double changeInEncValue) {
+        aim(aimMotor.goToPos + changeInEncValue);
+    }
+    
     public boolean iseenote() {
         return !note.get();
     }
@@ -85,6 +94,7 @@ public class Launch {
     public void update() {
 
         aimMotor.update();
+        feed.update();
 
         // Intake System:
         if (stage == 1) { // Intake note until detected
@@ -98,19 +108,24 @@ public class Launch {
             if (!iseenote()) { // Stop intake (finish process)
                 feed.set(0);
                 stage = 0;
+                holdingNote = true;
             }
         }
 
         // Launch System:
-        if (stage == 11) { // Fire up thrusters
+        if (stage == 11) { // Pull note in
+            feed.goTo(feed.getEnc() - 0.5535);
+            stage = 12;
+        }
+        if (stage == 12) { // Fire up thrusters
             leftThruster.set(2);
             rightThruster.set(0.8);
             if (launchTimer.get() > 1000) {
                 launchTimer.reset();
-                stage = 12;
+                stage = 13;
             }
         }
-        if (stage == 12) { // Push note into thrusters
+        if (stage == 13) { // Push note into thrusters
             leftThruster.set(2);
             rightThruster.set(0.8);
             feed.set(2);
@@ -119,6 +134,7 @@ public class Launch {
                 leftThruster.set(0);
                 rightThruster.set(0);
                 stage = 0;
+                holdingNote = false;
             }
         }
 
