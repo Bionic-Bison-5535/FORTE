4bd225ae664845310b141e7ca9c480433876d513
Wesley McGinn <wesleymcginn1@gmail.com>
2024-02-28 22:21:07 -0500

    Add testPeriodic for testing launchers

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -384,6 +384,10 @@ public class Robot extends TimedRobot {
     }
 
     @Override
-    public void testPeriodic() {}
+    public void testPeriodic() {
+        leftThruster.set(1);
+        rightThruster.set(1);
+        feedMotor.set(1);
+    }
 
 }
