e3e790e595dda4ed0cf9b1abda0ec7f8224e3343
Wesley McGinn <wesleymcginn1@gmail.com>
2024-03-09 18:39:58 -0500

    Allow co-driver to rotate bot

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -307,7 +307,7 @@ public class Robot extends TimedRobot {
             while (newAngle < dir - 180) { newAngle += 360; }
             dir = newAngle;
         } else if (c1.active() || c2.active()) { // Manual Rotation
-            dir += 1.7 * Math.pow(c1.stick(4), sensitivity);
+            dir += 1.7 * Math.pow(c1.stick(4) + c2.stick(4), sensitivity);
         }
         if (c1.left_stick() || c2.left_stick()) { // Turbo mode
             go.speed = go.default_speed;
