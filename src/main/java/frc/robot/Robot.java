44597e1272eb13d63e27d3546588db2c7d67b0c0
Wesley McGinn <wesleymcginn1@gmail.com>
2024-03-13 19:19:52 -0400

    Fix Center Auto

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -183,7 +183,7 @@ public class Robot extends TimedRobot {
             }
         }
         if (autoStage == 3) {
-            go.swerve(0.12, 0, 0, 0);
+            go.swerve(-0.12, 0, 0, 0);
             if (launcher.holdingNote) {
                 launcher.aimAndLAUNCH();
                 autoStage = 4;
@@ -194,11 +194,11 @@ public class Robot extends TimedRobot {
         }
         if (autoStage == 4) {
             if (noteToGet == "2" || speaker.valid()) {
-                go.swerve(0.15, 0, speaker.X()/40, 0);
+                go.swerve(0.1, 0, speaker.X()/40, 0);
             } else if (noteToGet == "1") {
-                go.swerve(0.15, 0, speaker.X()/40, 0);
+                go.swerve(0.1, 0, speaker.X()/40, 0);
             } else if (noteToGet == "3") {
-                go.swerve(0.15, 0, speaker.X()/40, 0);
+                go.swerve(0.1, 0, speaker.X()/40, 0);
             }
             if (!launcher.holdingNote) {
                 autoStage = 8;
